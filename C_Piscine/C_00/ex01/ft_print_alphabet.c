7d4db45d4b81f0c5ce4e69c03ca60a032b759b8d
Tarcisio Silva <76399323+Tarcisio2code@users.noreply.github.com>
2023-09-21 16:17:39 +0200

    Using a While Loop to display the a alphabet

---
+++ C_Piscine/C_00/ex01/ft_print_alphabet.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsilva <tsilva@student.42berlin.de>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/19 17:53:58 by tsilva            #+#    #+#             */
+/*   Updated: 2023/07/26 10:18:41 by tsilva           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_alphabet(void)
+{
+	char	letter;
+
+	letter = 'a';
+	while (letter <= 'z')
+	{
+		write(1, &letter, 1);
+		letter++;
+	}
+}
+//int	main(void) {ft_print_alphabet();}
