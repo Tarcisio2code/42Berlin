a9c1d527686098c1b576ca9db3d6a40741246134
Tarcisio Silva <76399323+Tarcisio2code@users.noreply.github.com>
2023-09-21 16:52:48 +0200

    Using a While Loop to display the a alphabet in reverse order

---
+++ C_Piscine/C_00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsilva <tsilva@student.42berlin.de>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/19 17:43:08 by tsilva            #+#    #+#             */
+/*   Updated: 2023/07/26 10:24:06 by tsilva           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	letter;
+
+	letter = 'z';
+	while (letter >= 'a')
+	{
+		write(1, &letter, 1);
+		letter--;
+	}
+}
+//int 	main(void) {ft_print_reverse_alphabet();}
