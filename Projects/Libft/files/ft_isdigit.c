37a1d67399b49263dc62c51fd9817a474625b0fa
Tarcisio Silva <76399323+Tarcisio2code@users.noreply.github.com>
2024-01-12 16:54:56 +0100

    Check whether the provided integer represents a digit character

---
+++ Projects/Libft/files/ft_isdigit.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isdigit.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsilva <tsilva@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/13 12:32:28 by tsilva            #+#    #+#             */
+/*   Updated: 2023/11/14 15:07:16 by tsilva           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_isdigit(int c)
+// c: represents an ASCII value to be checked
+{
+	// Checks if (c) lies within the ASCII range of digits (0-9)
+	if (!(c >= 48 && c <= 57))
+		//if (c) is not a digit, the function returns 0 (false)
+		return (0);
+	return (1);
+}
