619b1fada90061bbbf91a820449fa9ed9e297434
Tarcisio Silva <76399323+Tarcisio2code@users.noreply.github.com>
2024-07-08 11:38:22 +0200

    Writes a single character to a specified file descriptor

---
+++ Projects/Libft/files/ft_putchar_fd.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putchar_fd.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsilva <tsilva@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/25 11:06:12 by tsilva            #+#    #+#             */
+/*   Updated: 2023/11/25 11:12:43 by tsilva           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+/*
+ * c: The character to be written.
+ * fd: The file descriptor where the character will be written.
+ *
+ * This function uses the `write` system call to write the character `c` to the file descriptor `fd`.
+*/
+void ft_putchar_fd(char c, int fd)
+{
+    // Use the `write` system call to write the character `c` to the file descriptor `fd`.
+    write(fd, &c, 1);
+}
