882b400580abdc45724e66bc4eae226bad97f9a1
Tarcisio Silva <76399323+Tarcisio2code@users.noreply.github.com>
2024-07-08 14:30:20 +0200

    Writes a string to a specified file descriptor

---
+++ Projects/Libft/files/ft_putstr_fd.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsilva <tsilva@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/25 11:15:07 by tsilva            #+#    #+#             */
+/*   Updated: 2023/11/28 14:39:45 by tsilva           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+/*
+ * s: The string to be written.
+ * fd: The file descriptor where the string will be written.
+ *
+ * This function uses `ft_putchar_fd` to write each character of the string `s` to the file descriptor `fd`.
+*/
+void ft_putstr_fd(char *s, int fd)
+{
+    // Iterate through each character of the string `s`.
+    while (*s)
+    {
+        // Write the current character to the file descriptor `fd` using `ft_putchar_fd`.
+        ft_putchar_fd(*s++, fd);
+    }
+}
