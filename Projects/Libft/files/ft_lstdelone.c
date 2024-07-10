e8d48b8ed7ef04476dfff15214cb931094dff32a
Tarcisio Silva <76399323+Tarcisio2code@users.noreply.github.com>
2024-07-10 17:58:28 +0200

    Deletes a single node from a list

---
+++ Projects/Libft/files/ft_lstdelone.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsilva <tsilva@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/30 17:03:31 by tsilva            #+#    #+#             */
+/*   Updated: 2023/11/30 18:01:36 by tsilva           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+/*
+ * @lst: The node to be deleted.
+ * @del: A function pointer to delete the content of the node.
+ *
+ * This function deletes the node `lst` using the function `del` to free its content, and then frees the node itself.
+*/
+void ft_lstdelone(t_list *lst, void (*del)(void*))
+{
+    // Check if the node to be deleted and the delete function are not NULL.
+    if (lst && del)
+    {
+        // Call the delete function to free the content of the node.
+        (del)(lst->content);
+
+        // Free the memory allocated for the node itself.
+        free(lst);
+    }
+}
